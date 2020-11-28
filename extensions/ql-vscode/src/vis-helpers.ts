b0fe56c6d1d6ddf77257d4b1e431707e2e58c2b6
Robert MacGillivray <macgillivray.robert@gmail.com>
2020-11-28 13:46:07 -0700

    Update vis-helpers.ts
    - function stub to convert JSON to our node type

--- extensions/ql-vscode/src/vis-helpers.ts
+++ extensions/ql-vscode/src/vis-helpers.ts
@@ -4,3 +4,17 @@ export type TaintVisNode = {
   tainted: boolean;
   children: TaintVisNode[];
 }
+
+export function jsonToTaintVisNode(json: string) {
+  console.log(json);
+
+  // TODO: Build out TaintVisNode objects from the JSON, and associate them with the returned root
+  const root: TaintVisNode = {
+    uniqueID: '',
+    label: '',
+    tainted: false,
+    children: []
+  };
+
+  return root;
+}
